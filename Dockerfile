7ac7d951a5c1579ac9a09932c480a39b303dbe56
vivostanoev <vivostanoev@gmail.com>
2020-12-03 03:02:17 +0200

    ready to testing on Jenkins

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM openjdk:8-jre-slim
+
+#need to verify working WORKDIR
+WORKDIR C:/Users/Viliyan_Stanoev/IdeaProjects/seleniumDocker
+
+# Add the project jar & copy dependencies
+ADD  target/selenium-docker.jar selenium-docker.jar
+ADD  target/selenium-docker-tests.jar selenium-docker-tests.jar
+ADD  target/libs libs
+
+ENTRYPOINT java -cp "seleniumDocker-0.0.1-SNAPSHOT.jar;libs/*" -DseleniumHubHost=localhost org.junit.runner.JUnitCore seleniumDocker.OpenBrowserTests
---
+++ src/seleniumDocker/Base.java
@@ -0,0 +1,52 @@
+package seleniumDocker;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ErrorCollector;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+
+
+public class Base {
+
+		WebDriver driver;
+
+	    @Rule
+	    public ErrorCollector errorCollector = new ErrorCollector();
+	    
+	    @Rule
+	    public seleniumDocker.LoggerRule log = new LoggerRule();
+
+	    @Before
+	    public void beforeTest() throws MalformedURLException {
+
+
+	        DesiredCapabilities dc = DesiredCapabilities.chrome();
+
+	        String host = System.getProperty("seleniumHubHost");
+
+			System.out.println("-------------------------------------------------"+ host);
+
+	        driver = new RemoteWebDriver(new URL("http://" + host + ":4444/wd/hub"), dc);
+			WebDriverRunner.setWebDriver(driver);
+
+	        Selenide.open("https://www.demoblaze.com/");
+	        //driver.get("https://www.demoblaze.com/");
+
+	    }
+
+
+	@After
+	    public synchronized void afterTest()
+	    {
+	        Selenide.closeWebDriver();
+	    }
+}
