1eeba4dedf2d931e7fbab9dbadb1e5e7070d0742
vivostanoev <vivostanoev@gmail.com>
2020-12-11 16:44:25 +0200

    add a display name

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,15 +1,25 @@
 package test.java;
 
+import com.codeborne.selenide.Screenshots;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.junit.ScreenShooter;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.google.common.io.Files;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.ErrorCollector;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,15 +27,17 @@ import java.net.URL;
 public class Base {
 
 	@Rule
-	    public ErrorCollector errorCollector = new ErrorCollector();
+	public ErrorCollector errorCollector = new ErrorCollector();
 	    
-	    @Rule
-	    public LoggerRule log = new LoggerRule();
-		@Rule
-		public ScreenShooter makeScreenshotOnFailure = ScreenShooter.failedTests();
+	@Rule
+	public LoggerRule log = new LoggerRule();
 
-	    @Before
+
+
+	@Before
 	    public void beforeTest() throws MalformedURLException {
+
+			SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
 
 			ChromeOptions dc = new ChromeOptions();
 
@@ -43,9 +55,16 @@ public class Base {
 
 
 	@After
-	    public synchronized void afterTest()
-	    {
+	    public synchronized void afterTest() throws IOException {
+			screenshot();
 	        log.printf();
 	    	Selenide.closeWebDriver();
 	    }
+
+
+	@Attachment(type = "image/png")
+	public byte[] screenshot() throws IOException {
+		File screenshot = Screenshots.getLastScreenshot();
+		return Files.toByteArray(screenshot);
+	}
 }
